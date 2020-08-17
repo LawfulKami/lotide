b55391332ead7aa2cca58966bf33caa4302a3c9d
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-17 20:11:46 +0000

    assertEqual.js added

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 3);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
