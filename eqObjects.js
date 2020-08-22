fba343fbe144d8d4087c5360f9bb71fbd972f2d2
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-22 13:20:24 -0400

    Refact with Recursions

--- eqObjects.js
+++ eqObjects.js
@@ -19,6 +19,8 @@ const eqObjects = function(object1, object2) {
   for (let prop in object1) {
     if (Array.isArray(object1[prop]) && Array.isArray(object2[prop])) {
       if (!eqArrays(object1[prop], object2[prop])) return false;
+    } else if (typeof object1[prop] === "object" && typeof object2[prop] === "object") {
+      if (!eqObjects(object1[prop], object2[prop])) return false;
     } else {
       if (object1[prop] !== object2[prop]) {
         return false;
@@ -28,6 +30,8 @@ const eqObjects = function(object1, object2) {
   for (let prop in object2) {
     if (Array.isArray(object1[prop]) && Array.isArray(object2[prop])) {
       if (!eqArrays(object1[prop], object2[prop])) return false;
+    } else if (typeof object1[prop] === "object" && typeof object2[prop] === "object") {
+      if (!eqObjects(object1[prop], object2[prop])) return false;
     } else {
       if (object1[prop] !== object2[prop]) {
         return false;
@@ -54,3 +58,8 @@ assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { z: 1 }, b: 3 }, { a: { z: 1 }, b: 2 }), false); // => false
