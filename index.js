14f1b39fd3c92bd9d581b5787364c1c7ad5780f3
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-24 14:31:28 -0400

    Index shortened

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const without = require("./without");
 
 module.exports = {
 
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  eqArrays: eqArrays,
-  eqObjecta: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  head  : head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail  : tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 
 };
