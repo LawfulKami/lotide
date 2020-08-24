469be62a34907766dcf6264d92b6c3579e83bc1f
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-24 14:04:22 -0400

    Starting the index

--- test/TailTest.js
+++ test/TailTest.js
@@ -1,14 +1,11 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
 
-const empty = tail(["Hello"]);
-assertEqual(empty.length, 0);
+describe("#tail", () => {
+
+  it("should return [\"Lighthouse\", \"Labs\"] for, [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,9 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+
+  it("should return [3, 4] for, [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
